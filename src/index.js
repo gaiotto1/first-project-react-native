42dc75456c422becee7c07fa0c0b8f96e649d6eb
Julio Cesar Aparecido Gaiotto <julio.gaiotto@totvs.com.br>
2020-01-30 18:24:39 -0300

    iniciando com styled components

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Main from './pages/Main';
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <Routes />
     </>
   );
--- src/routes.js
+++ src/routes.js
@@ -19,7 +19,7 @@ const Routes = createAppContainer(
         },
         headerTintColor: '#fff',
       },
-    }
+    },
   ),
 );
 
