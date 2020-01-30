f16b87b970af9a9368e9e7dcac7552f4f4015eec
Julio Cesar Aparecido Gaiotto <julio.gaiotto@totvs.com.br>
2020-01-30 18:12:17 -0300

    configurando a StatusBar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
@@ -6,7 +7,12 @@ import Routes from './routes';
 import Main from './pages/Main';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+      <Routes />
+    </>
+  );
 }
 
 Main.navigationOptions = {
