1b0212e815231e0bb1e80f1b0c8f6c4b233a120f
Julio Cesar Aparecido Gaiotto <julio.gaiotto@totvs.com.br>
2020-02-03 15:45:46 -0300

    listando os repositórios favoritos

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import { Container, Header, Avatar, Name, Bio, Stars, Starred, OwnerAvatar, Info, Title, Author } from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -31,7 +30,32 @@ export default class User extends Component {
 
   render() {
     const { stars } = this.state;
+    // const user = this.props.navigation.getParam('user');
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
 
-    return <View />;
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars} // stars é um array
+          keyExtractor={star => String(star.id)}
+          renderItem={({item}) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
   }
 };
